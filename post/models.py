114832929046fd293cfe04e396c63c9af1bfd7b8
heyhyo11 <1326cy@gmail.com>
2022-07-21 13:56:02 +0900

    feat: DB 모델링 - delete_flag 추가 (#1)

--- post/models.py
+++ post/models.py
@@ -9,8 +9,15 @@ class Post(models.Model):
     hashtag = models.CharField('해시태그', max_length=255)
     hits = models.PositiveIntegerField('조회수', default=0)
     likes = models.PositiveBigIntegerField('좋아요', default=0)
+    delete_flag = models.BooleanField('삭제 상태', default=False)  # True: 삭제
     created_at = models.DateTimeField('생성일', auto_now_add=True)
     updated_at = models.DateTimeField('수정일', auto_now=True)
 
     def __str__(self):
         return f'{self.title} / {self.writer}'
+
+    def toggle_active(self):
+        self.delete_flag = not self.delete_flag
+        self.save()
+        message = '삭제' if self.delete_flag else '복구'
+        self.delete_message = {'success': f'{message}되었습니다.'}
